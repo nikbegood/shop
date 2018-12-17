1f19768c6096ab7aab913caf35da19842ade66ca
nikbegood <nikita.tsymbalenko@mail.ru>
2018-12-17 17:04:27 +0300

    First commit

---
+++ views/layouts/footer.php
@@ -0,0 +1,11 @@
+<footer id="footer" class="page-footer"><!--Footer-->
+    
+</footer><!--/Footer-->
+
+
+
+<script src="#"></script>
+
+
+</body>
+</html>
---
+++ views/layouts/header_admin.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="description" content="">
+        <meta name="author" content="">
+        <title>Админпанель</title>
+        <link href="/template/css/main.css" rel="stylesheet">
+
+        <!--[if lt IE 9]>
+        <script src="js/html5shiv.js"></script>
+        <script src="js/respond.min.js"></script>
+        <![endif]-->       
+        <link rel="shortcut icon" href="#">
+        <link rel="apple-touch-icon-precomposed" sizes="144x144" href="/">
+        <link rel="apple-touch-icon-precomposed" sizes="114x114" href="/">
+        <link rel="apple-touch-icon-precomposed" sizes="72x72" href="/">
+        <link rel="apple-touch-icon-precomposed" href="/">
+    </head><!--/head-->
+
+   
+               
